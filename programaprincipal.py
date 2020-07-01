f8c802edd89456271980b8b9ecd7ab666f3363bb
nicolasmmartino@hotmail.com <nicolasmmartino@hotmail.com>
2020-07-01 01:41:11 -0300

    Ordenamiento archivos programas.txt

--- programaprincipal.py
+++ programaprincipal.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 #Ejercicio 3
 #Escribir una función que reciba una dirección de mail, y devuelva True ó False, en función de haber evaluado que dicha dirección está bien formada.
 #Escribir una función que reciba una cadena de caracteres que representa una dirección de mail. La función deberá devolver True ó False, en función de  haber evaluado que dicha dirección está bien formada.
@@ -7,9 +6,7 @@
 #a.	Que no contenga blancos
 #b.	Que sólo se utilicen letras y/o números para la parte del nombre, delante de la @
 #c.	Que haya exactamente una arroba'''
-
 def validacion_mail(email):
-
     if ' ' in email:
         valido = False 
     elif email.count('@') != 1:
@@ -20,7 +17,4 @@ def validacion_mail(email):
         valido = False
     else:
         valido = True
-
     return valido
-    
-
--- punto1.py
+++ punto1.py
@@ -1,29 +1,61 @@
 #!/usr/bin/env python3
 import os
-
-def leer(archivo):
-    linea = archivo.readline()
+minimo=0
+def convertir_linea_iterable(archivo,condicion_corte):
+    linea = leer_linea_string(archivo)
     if linea:
-        devolver = linea.rstrip("\n").split(",")
+        devolver = linea.split(condicion_corte)
     else:
-        devolver = ""
+        devolver = minimo
     return devolver
-
+def leer_linea_string(archivo):
+    #Esta función lee el archivo pero no lo convierte en iterable
+    return archivo.readline().rstrip("\n") 
 def guarda_codigo():
+    #Esta funcion guardará cada archivo ordenado en una lista
+    lista_archivos=[]
     with open("programas.txt", 'r') as programa:
-        linea = leer(programa)
+        linea = leer_linea_string(programa)
         while linea:
-            with open(linea[0],'r') as codigo:
-                with open("fuente_unico.csv",'w') as fuente_unico:
-                    linea_fuente = leer(codigo)
-                    while linea_fuente:
-                        fuente.write("".join(linea_fuente))
-                        linea_fuente = leer(codigo)
-            linea = leer(programa)
+            with open(linea,'r') as codigo:
+                archivo_ordenado=ordenar_archivo(codigo)
+                lista_archivos.append(archivo_ordenado)
+            linea = leer_linea_string(programa)
+    return lista_archivos
+def merge(lista_archivos):
+    cantidad_archivos=len(lista_archivos)
+    y=0
+    for x in range (cantidad_archivos):
+
+
+
+
+
+
+
+
+
+
+
+def cargar_memoria_archivo(archivo):
+    #Esta funcion convierte el archivo entero en un iterable
+    linea=leer_linea_string(archivo).strip()
+    archivo_cargado=[]
+    while linea:
+        archivo_cargado.append(linea)
+        linea=leer_linea_string(archivo).strip()
+    return archivo_cargado
+def ordenar_archivo(archivo):
+    #Esta función ordena el archivo
+    archivo_cargado=cargar_memoria_archivo(archivo)
+    archivo_ordenado=sorted(archivo_cargado)
+    return archivo_ordenado
+
                     
 
 
-guarda_codigo()
+print(guarda_codigo())
+
 
 
 
