5dc3fd19ab9106208bd79ba41c95d742da02e155
alejandrodelcs <alejodc15@gmail.com>
2020-07-06 04:42:12 -0300

    Falta laburar en la mezcla

--- punto1.py
+++ punto1.py
@@ -1,38 +1,49 @@
 #!/usr/bin/env python3
-'''def contar_sentencias(lista_codigo):
-    cont_if = 0
-    cont_for = 0
-    cont_while = 0
-    cont_return = 0
-    cont_elif = 0
-    for codigo in lista_codigo:
-        print(codigo)
-        if 'if' in codigo:
-            cont_if += 1
-        elif 'for' in codigo:
-            cont_for += 1
-        elif 'while' in codigo:
-            cont_while += 1
-        elif 'return' in codigo:
-            cont_return += 1
-        elif 'elif' in codigo:
-            cont_elif += 1
-    return cont_if, cont_for,cont_while, cont_return, cont_elif PARA DESPUES AMEO'''
-
-
-
-def archivo_lista(lista_codigo,lista_codigo_2):
-    lista_codigo_ordenado = sorted(lista_codigo)
-    lista_codigo_2_ordenado = sorted(lista_codigo_2)
-    
 
 def leer_rutas(archivo):
+    """
+    leer el archivo programas.txt
+    """
     linea = archivo.readline()
     if linea:
         devolver = linea.rstrip('\n') 
     else:
         devolver = ""
     return devolver
+
+def grabar_nuevo(archivoentero,linea_cod_general):
+    archivoentero.write(linea_cod_general+'\n')
+
+def mezcla():
+    with open("archivo1.txt") as archivo1, open("archivo2.txt") as archivo2, open('archivoentero.txt','w') as archivoentero:
+        linea_cod1 = leer_rutas(archivo1)
+        linea_cod2 = leer_rutas(archivo2)
+        while linea_cod1 and linea_cod2:
+            if linea_cod1 ==  linea_cod2:
+                grabar_nuevo(archivoentero,linea_cod1)
+                grabar_nuevo(archivoentero,linea_cod2)
+                linea_cod1 = leer_rutas(archivo1)
+                linea_cod2 = leer_rutas(archivo2)
+            elif linea_cod1[:1]<linea_cod2[:1]:
+                grabar_nuevo(archivoentero,linea_cod1)
+                linea_cod1 = leer_rutas(archivo1)
+            else:#linea_cod1[:1]>linea_cod2[:1]:
+                grabar_nuevo(archivoentero,linea_cod2)
+                linea_cod2 = leer_rutas(archivo2)
+
+
+def crear_archivos_ordenados(l_codigo1,l_codigo2):
+    """"
+    Recibe dos listas para luego ordenarlas y finalmente cargarlas en dos archivos
+    """
+    l_codigo_ordenado = sorted(l_codigo1)
+    l_codigo2_ordenado = sorted(l_codigo2)
+    with open('archivo1.txt','w') as archivo1, open('archivo2.txt','w') as archivo2:
+        for linea1,linea2 in zip(l_codigo_ordenado,l_codigo2_ordenado):
+            if linea1 != ''and linea2 != '':
+                archivo1.write(linea1+'\n')
+                archivo2.write(linea2+'\n')
+
 
 def listar_archivo():
     lista_codigo = []
@@ -48,10 +59,11 @@ def listar_archivo():
                     linea_codigo = codigo.readline()
             pos = lista_codigo.index(ruta)
             ruta = leer_rutas(programa)
-        archivo_lista(lista_codigo[0:pos],lista_codigo[pos:])
+        crear_archivos_ordenados(lista_codigo[0:pos],lista_codigo[pos:])
     
 
 
 
 
 listar_archivo()
+mezcla()
