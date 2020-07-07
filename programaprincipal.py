24c60ad5e4eb68e159dc3e39b04838e4f3f80172
nicolasmmartino@hotmail.com <nicolasmmartino@hotmail.com>
2020-07-07 02:24:50 -0300

    Reorganización creacion de archivos fuente unico y comentarios

--- programaprincipal.py
+++ programaprincipal.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-#Ejercicio 3
-#Escribir una función que reciba una dirección de mail, y devuelva True ó False, en función de haber evaluado que dicha dirección está bien formada.
-#Escribir una función que reciba una cadena de caracteres que representa una dirección de mail. La función deberá devolver True ó False, en función de  haber evaluado que dicha dirección está bien formada.
-#Se debe controla que: 
-#a.	Que no contenga blancos
-#b.	Que sólo se utilicen letras y/o números para la parte del nombre, delante de la @
-#c.	Que haya exactamente una arroba'''
-def validacion_mail(email):
+# Ejercicio 3
+# Escribir una función que reciba una dirección de mail, y devuelva True ó False, en función de haber evaluado que dicha dirección está bien formada.
+# Escribir una función que reciba una cadena de caracteres que representa una dirección de mail. La función deberá devolver True ó False, en función de  haber evaluado que dicha dirección está bien formada.
+# Se debe controla que: 
+# a.	Que no contenga blancos
+# b.	Que sólo se utilicen letras y/o números para la parte del nombre, delante de la @
+# c.	Que haya exactamente una arroba'''
+def validacion_mail (email) :
     if ' ' in email:
         valido = False 
     elif email.count('@') != 1:
