16dedd30a6fd5bfb9ac363cac7854922aff95624
alejandrodelcs <alejodc15@gmail.com>
2020-07-13 18:13:32 -0300

    Merge para N archivos

--- mezcla.py
+++ mezcla.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python3
+import csv
+from os import remove
 def leer_archivos_csv (archivocsv):
     """Autor : Alejandro"""
     """Ayuda : leer archivos .csv y devuelve una lista de ellos para un mejor estudio"""
     linea = archivocsv.readline()
     return linea.rstrip('\n').split(',') if linea else ""
-def grabar_nuevo (archivo,linea):
-    """Autor : Alejandro"""
-    """Ayuda : genera un archivo unificado, es decir archivo2.csv+archivo1.csv"""
-    archivo.write(",".join(linea) +'\n')
-def tipo_archivos (archivo1):
+def tipo_archivos (archivo):
     """Autor : Alejandro"""
     """Ayuda : valida si el archivo recibido es comentarios o fuente_unico"""
-    if "comentarios" in archivo1:
-        archivo3 = "comentarios_unificado.csv"
+    if "comentarios" in archivo:
+        archivo_unico = "comentarios_unificado.csv"
     else:
-        archivo3 = "fuente_unificado.csv"
-    return archivo3
-def merge (archivo1,archivo2):
+        archivo_unico = "fuente_unificado.csv"
+    return archivo_unico
+def guarda_archivo (archivo_aux,lista_archivos):
+    """Autor: Alejandro"""
+    """Lee el archivo auxiliar y vuelve a generar otro, eliminando luego el anterior"""
+    archivo_mezcla = tipo_archivos(lista_archivos[0])
+    with open(archivo_mezcla,"w") as prestaciones:
+        with open(archivo_aux,"r") as auxiliar:
+            entrada = csv.reader(auxiliar)
+            ordenado = sorted(entrada, key=lambda fila: fila[0])
+            for fila in ordenado:
+                salida = csv.writer(prestaciones)
+                salida.writerow(fila)
+    remove(archivo_aux)
+def merge (lista_archivos):
     """Autor : Alejandro"""
-    """Ayuda : unifica archivo1.csv y archivo2.csv en un solo archivo"""
-    archivo3 = tipo_archivos(archivo1)
-    with open(archivo1,"r") as archivo1, open(archivo2,"r") as archivo2,\
-        open(archivo3,"w") as archivo_mezcla:
-            linea_archivo1 = leer_archivos_csv(archivo1)
-            linea_archivo2 = leer_archivos_csv(archivo2)
-            while linea_archivo1 and linea_archivo2:
-                if linea_archivo1[0] ==  linea_archivo2[0]:
-                    grabar_nuevo(archivo_mezcla,linea_archivo2)
-                    grabar_nuevo(archivo_mezcla,linea_archivo1)
-                    linea_archivo1 = leer_archivos_csv(archivo1)
-                    linea_archivo2 = leer_archivos_csv(archivo2)
-                elif linea_archivo1[0] < linea_archivo2[0]:
-                    grabar_nuevo(archivo_mezcla,linea_archivo1)
-                    linea_archivo1 =leer_archivos_csv(archivo1)
-                else:
-                    grabar_nuevo(archivo_mezcla,linea_archivo2)
-                    linea_archivo2 = leer_archivos_csv(archivo2)
-    return archivo3
+    """Mezcla Archivos CSV's"""
+    archivo_aux="archivo_aux.csv"
+    with open(archivo_aux,"w") as unificado:
+        for archivo in lista_archivos:
+            with open(archivo,'r') as arch:
+                linea = leer_archivos_csv(arch)
+                while linea:
+                    entrada = csv.writer(unificado)
+                    entrada.writerow(linea)
+                    linea = leer_archivos_csv(arch)
+    guarda_archivo(archivo_aux,lista_archivos)
