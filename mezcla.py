73431f1c3e669af3eddb4248884a3d8a9c4c5513
alejandrodelcs <alejodc15@gmail.com>
2020-07-13 23:39:02 -0300

    terminado y funcionando

--- mezcla.py
+++ mezcla.py
@@ -2,21 +2,21 @@
 import csv
 from os import remove
 def leer_archivos_csv (archivocsv):
-    """Autor : Alejandro"""
-    """Ayuda : leer archivos .csv y devuelve una lista de ellos para un mejor estudio"""
+    """ Autor : Alejandro """
+    """ Ayuda : leer archivos .csv y devuelve una lista de cada linea del archivo """
     linea = archivocsv.readline()
     return linea.rstrip('\n').split(',') if linea else ""
 def tipo_archivos (archivo):
-    """Autor : Alejandro"""
-    """Ayuda : valida si el archivo recibido es comentarios o fuente_unico"""
+    """ Autor : Alejandro """
+    """ Ayuda : valida si el archivo recibido es comentarios o fuente_unico """
     if "comentarios" in archivo:
-        archivo_unico = "comentarios_unificado.csv"
+        archivo_unico = "comentarios.csv"
     else:
-        archivo_unico = "fuente_unificado.csv"
+        archivo_unico = "fuente_unico.csv"
     return archivo_unico
 def guarda_archivo (archivo_aux,lista_archivos):
-    """Autor: Alejandro"""
-    """Lee el archivo auxiliar y vuelve a generar otro, eliminando luego el anterior"""
+    """ Autor : Alejandro"""
+    """ Ayuda : Lee el archivo auxiliar y vuelve a generar otro, eliminando luego el anterior """
     archivo_mezcla = tipo_archivos(lista_archivos[0])
     with open(archivo_mezcla,"w") as prestaciones:
         with open(archivo_aux,"r") as auxiliar:
@@ -27,8 +27,8 @@ def guarda_archivo (archivo_aux,lista_archivos):
                 salida.writerow(fila)
     remove(archivo_aux)
 def merge (lista_archivos):
-    """Autor : Alejandro"""
-    """Mezcla Archivos CSV's"""
+    """ Autor : Alejandro """
+    """ Ayuda : Mezcla Archivos CSV's """
     archivo_aux="archivo_aux.csv"
     with open(archivo_aux,"w") as unificado:
         for archivo in lista_archivos:
