13bf51325e90f59467ecb0d43aebdf3d6eaebe6a
alejandrodelcs <alejodc15@gmail.com>
2020-07-17 20:09:45 -0300

    este es la tabla del punto 5 nuevamente el formato

--- tabla2.py
+++ tabla2.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 def imprimir(dicc_desarrolladores,total_lineas):
+    total_desarrollador = 0
     with open("participacion.txt","w") as informe:
         total_funciones = 0
         linea = '-'*50
         print('\n\tInforme de Desarrollo por Autor\n')
+        print(linea)
         informe.write('\n\tInforme de Desarrollo por Autor\n'+'\n')
+        informe.write(linea)
+
         for autor,funciones in dicc_desarrolladores.items():
+            acum_lineas = 0
+            cant_funciones_desarrollador = 0
             print('\nAutor:  {}\n'.format(autor))
             informe.write('\nAutor:  {}\n'.format(autor))
-            print('Función\t\tLineas')
-            informe.write('Función\t\tLineas'+'\n')
-            print(linea)
+            print('\tFunción\t\tLineas')
+            informe.write('\tFunción\t\tLineas'+'\n')
             informe.write(linea+'\n')
             lista_funciones = sorted(funciones, key= lambda funcion:funcion[1])
             total_funciones += len(lista_funciones)
             for data in lista_funciones:
-                print("{}\t{}".format(data[0],data[1]))
+                cant_funciones_desarrollador += 1
+                acum_lineas += data[1]
+                print("\t{}\t{}".format(data[0],data[1]))
                 informe.write("{}\t\t{}".format(data[0],data[1])+'\n')
-        print('Total Funciones {} - Lineas {}'.format(total_funciones,total_lineas))
-        informe.write('Total Funciones {} - Lineas {}'.format(total_funciones,total_lineas)+'\n')
+                porcentaje=(acum_lineas*100)//total_lineas
+            print("\t{} Funciones-lineas\t{}\t{}%".format(cant_funciones_desarrollador,acum_lineas,porcentaje))
+            informe.write("\t{}Funciones-Lineas\t{}\t{}%".format(cant_funciones_desarrollador,acum_lineas,porcentaje))
+        print('\nTotal Funciones {} - Lineas {}'.format(total_funciones,total_lineas))
+        informe.write('\n\nTotal Funciones {} - Lineas {}'.format(total_funciones,total_lineas)+'\n')
