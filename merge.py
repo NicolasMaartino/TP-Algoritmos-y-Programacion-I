d7cf95669be841f04384807d7b001a5af3ac12e9
alejandrodelcs <alejodc15@gmail.com>
2020-07-18 05:12:22 -0300

    Realiza el merge de manera pedida

--- merge.py
+++ merge.py
@@ -1,5 +1,49 @@
-from generales import buscar_dato,unir_linea,reemplazar_toda_la_lista,reemplazar_string,ordenamiento_insercion 
+#!/usr/bin/env python3
+import csv
+from os import remove
+from generales import buscar_dato,unir_linea,reemplazar_toda_la_lista,reemplazar_string,ordenamiento_insercion,leer_archivo,tipo_archivos 
 from archivos import *
+
+def guarda_archivo_mezcla (archivo_aux,lista_archivos):
+    """ Autor : Alejandro"""
+    """ Ayuda : Lee archivo_aux.csv, extrae la informacion y realiza la mezcla respectiva para luego eliminar archivo_aux.csv"""
+    archivo_mezcla = tipo_archivos(lista_archivos[0])
+    with open(archivo_mezcla,"w") as prestaciones:
+        with open(archivo_aux,"r") as auxiliar:
+            entrada = csv.reader(auxiliar)
+            ordenado = sorted(entrada, key=lambda fila: fila[0])
+            for fila in ordenado:
+                salida = csv.writer(prestaciones)
+                salida.writerow(fila)
+    remove(archivo_aux) 
+
+def mezcla (lista_archivos):
+    """ Autor : Alejandro """
+    """ Ayuda : Mezcla Archivos CSV's """
+    archivo_aux = "archivo_aux.csv"
+    with open(archivo_aux,"w") as unificado:
+        for archivo in lista_archivos:
+            with open(archivo,'r') as arch:
+                linea = leer_archivo(arch)
+                while linea:
+                    entrada = csv.writer(unificado)
+                    entrada.writerow(linea)
+                    linea = leer_archivo(arch)
+    guarda_archivo_mezcla(archivo_aux,lista_archivos)
+
+def separa_comentarios_fuentes (lista_archivos):
+    """ Autor : Alejandro"""
+    """ Ayuda : Esta función recibe una lista con comentarios y fuente, divide en dos listas acorde al nombre respectivo"""
+    comentarios = []
+    fuente_unico = []
+    for ruta in lista_archivos:
+        if "comentarios" in ruta:
+            comentarios.append(ruta)
+        else:
+            fuente_unico.append(ruta)
+    mezcla(comentarios)
+    mezcla(fuente_unico)
+
 def linea_ayuda_autor (linea,linea_comentarios,linea_fuente,palabras_buscadas):
     """[Autor : Nicolas] """
     """[Ayuda : Se le pasa una lista con las palabras encontradas y se fija si realmente alguna esta]"""
@@ -36,9 +80,7 @@ def validar_linea (nombre_modulo,archivo) :
     funciones_fuente = [] # Aca iran a parar las funciones para fuente codigo 
     funciones_comentarios = [] # Y aca las funciones para comentarios
     ultima_lectura = leer_linea(archivo)
-    
     while ultima_lectura[0] != '""""""':
-        
         if ultima_lectura[0] == "def":
             #Analizaremos la funcion y la dividiremos en dos listas para saber a que archivo pertenecen.
             nombre_funcion = ultima_lectura[1]
@@ -48,12 +90,10 @@ def validar_linea (nombre_modulo,archivo) :
             linea_comentarios,linea_fuente,ultima_lectura=analizo_funcion(linea_fuente,linea_comentarios,archivo)
             funciones_fuente.append(linea_fuente)
             funciones_comentarios.append(linea_comentarios)
-        
         else:
             ultima_lectura=leer_linea(archivo)
     
     return ordenamiento_insercion(funciones_fuente),ordenamiento_insercion(funciones_comentarios)
-
 def analizo_funcion (linea_fuente,linea_comentarios,archivo):
     """[Autor : Nicolas ] """
     """[Ayuda : Analizara la funcion para enviarla a las listas correspondientes]"""
@@ -96,7 +136,6 @@ def seccion_comentarios (lectura,lista_comentarios,lista_fuente) :
         i+=1
     
     return lista_fuente,lista_comentarios
-
 def analiza_codigo () :
     """[ Autor : Nicolas]"""
     """[Ayuda : Guardara codigo como lo pide el enunciado]"""
@@ -107,70 +146,22 @@ def analiza_codigo () :
     rutas = open("programas.txt", 'r')
     ruta = leer_linea_string(rutas)
     i = 0
-    
     while ruta: # aaj
         i+=1 # Este indice lo creo para distinguir los archivos
         nombre_archivo = ruta.split("/").pop()
         
         # Abro ruta dentro de programas.txt
-        
         codigo = open(ruta,'r')
         fuente_unico,comentarios = validar_linea(nombre_archivo,codigo)
         ruta_fuente = "fuente_unico"+str(i) +".csv"
         ruta_comentarios= "comentarios"+str(i) +".csv"
-        
-        #Genero los archivos de cada ruta fuente_unico.csv y comentarios.csv.
-        # Luego se hace el merge
-        
         generar_archivo(fuente_unico,ruta_fuente)
         generar_archivo(comentarios,ruta_comentarios)
         lista_archivos.append(ruta_fuente)
         lista_archivos.append(ruta_comentarios)
         ruta = leer_linea_string(rutas)
-         
+    separa_comentarios_fuentes(lista_archivos)
     return lista_archivos
 
-def leer_archivos_csv (archivocsv):
-    """ Autor : Alejandro """
-    """ Ayuda : leer archivos .csv y devuelve una lista de cada linea del archivo """
-    linea = archivocsv.readline()
-    return linea.rstrip('\n').split(',') if linea else ""
-def tipo_archivos (archivo):
-    """ Autor : Alejandro """
-    """ Ayuda : valida si el archivo recibido es comentarios o fuente_unico """
-    if "comentarios" in archivo:
-        archivo_unico = "comentarios.csv"
-    else:
-        archivo_unico = "fuente_unico.csv"
-    return archivo_unico
-def guarda_archivo (archivo_aux,lista_archivos):
-    """ Autor : Alejandro"""
-    """ Ayuda : Lee el archivo auxiliar y vuelve a generar otro, eliminando luego el anterior """
-    archivo_mezcla = tipo_archivos(lista_archivos[0])
-    with open(archivo_mezcla,"w") as prestaciones:
-        with open(archivo_aux,"r") as auxiliar:
-            entrada = csv.reader(auxiliar)
-            ordenado = sorted(entrada, key=lambda fila: fila[0])
-            for fila in ordenado:
-                salida = csv.writer(prestaciones)
-                salida.writerow(fila)
-    remove(archivo_aux)
-def merge (lista_archivos):
-    """ Autor : Alejandro """
-    """ Ayuda : Mezcla Archivos CSV's """
-    archivo_aux="archivo_aux.csv"
-    with open(archivo_aux,"w") as unificado:
-        for archivo in lista_archivos:
-            with open(archivo,'r') as arch:
-                linea = leer_archivos_csv(arch)
-                while linea:
-                    entrada = csv.writer(unificado)
-                    entrada.writerow(linea)
-                    linea = leer_archivos_csv(arch)
-    guarda_archivo(archivo_aux,lista_archivos)
-
+analiza_codigo()
 """"""
-
-
-
-
