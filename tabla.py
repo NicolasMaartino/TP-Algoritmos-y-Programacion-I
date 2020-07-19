12ceb96aa8b6dafc0c7bea2a320f0fe00f235f55
alejandrodelcs <alejodc15@gmail.com>
2020-07-19 04:28:30 -0300

    se corrige tabla de informacion_desarrollada

--- tabla.py
+++ tabla.py
@@ -40,28 +40,7 @@ def tabla(archivo):
         linea=leer_archivo(archivo)
     return
 
-def imprimir(dicc_desarrolladores,total_lineas):
-    with open("participacion.txt","w") as informe:
-        total_funciones = 0
-        linea = '-'*50
-        print('\n\tInforme de Desarrollo por Autor\n')
-        informe.write('\n\tInforme de Desarrollo por Autor\n'+'\n')
-        for autor,funciones in dicc_desarrolladores.items():
-            print('\nAutor:  {}\n'.format(autor))
-            informe.write('\nAutor:  {}\n'.format(autor))
-            print('Función\t\tLineas')
-            informe.write('Función\t\tLineas'+'\n')
-            print(linea)
-            informe.write(linea+'\n')
-            lista_funciones = sorted(funciones, key= lambda funcion:funcion[1])
-            total_funciones += len(lista_funciones)
-            for data in lista_funciones:
-                print("{}\t{}".format(data[0],data[1]))
-                informe.write("{}\t\t{}".format(data[0],data[1])+'\n')
-        print('Total Funciones {} - Lineas {}'.format(total_funciones,total_lineas))
-        informe.write('Total Funciones {} - Lineas {}'.format(total_funciones,total_lineas)+'\n')
-
-def imprimir(dicc_desarrolladores,total_lineas):
+def imprimir_informacion_desarrollador(dicc_desarrolladores,total_lineas):
     total_desarrollador = 0
     with open("participacion.txt","w") as informe:
         total_funciones = 0
@@ -70,7 +49,6 @@ def imprimir(dicc_desarrolladores,total_lineas):
         print(linea)
         informe.write('\n\tInforme de Desarrollo por Autor\n'+'\n')
         informe.write(linea)
-
         for autor,funciones in dicc_desarrolladores.items():
             acum_lineas = 0
             cant_funciones_desarrollador = 0
