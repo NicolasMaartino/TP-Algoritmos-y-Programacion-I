85abbac1db9def428e32d05f7079f124441204ee
juanIgodoy <jigodoy@fi.uba.ar>
2020-07-19 04:07:39 -0300

    comiteo para poder cambiar de rama

--- tabla_1.py
+++ tabla_1.py
@@ -1,20 +1,3 @@
-
-#Mas o menos esta es mi idea para la tabla, faltan detalles y mejorarla visualmente(cosas del format, tengo que alinearlos y demas).
-#Si saben como hacer un codigo mas limpio, si hice algo mal o tienen alguna idea para mejorarlo, me dicen y lo hacemos en un toque.
-
-
-
-def leer_archivo(archivo):
-    """[Autor : Juan Godoy]"""
-    """[Ayuda : Funcion que lee lineas de un archivo]"""
-    linea=archivo.readline()
-    if linea:
-        lista=linea.rstrip("\n").split(",")
-    else:
-        lista=[""]
-    return lista
-
-
 def tabla(archivo):
     """[Autor : Juan Godoy]"""
     """[Ayuda : Funcion que acumula nombres de funciones para luego dibujarlas en una tabla]"""
