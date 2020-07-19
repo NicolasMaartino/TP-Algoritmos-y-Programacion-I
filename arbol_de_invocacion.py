db290a2959f99b85bb69132114c5b40b0fab7816
lucialiceri <63566999+lucialiceri@users.noreply.github.com>
2020-07-19 20:03:43 -0300

    Punto 4, falta

--- arbol_de_invocacion.py
+++ arbol_de_invocacion.py
@@ -1,34 +1,32 @@
-"""
-Diagrama que gráfica la interacción entre las funciones, indicando quien llama a qué función.
-El valor que se encuentra entre paréntesis es la cantidad de líneas que tiene la función.
-"""
-
-from reutilizacion_codigo import funciones_invocadas, crear_filas # La clave es la funcion que invoca y el valor es la funcion invocada
+from reutilizacion_codigo import funciones_invocadas, crear_filas 
 from generales import listar_archivo
 
-dic = {'analiza_codigo': {'generar_archivo': 2, 'leer_linea_string': 2, 'validar_linea': 1},
-       'generar_archivo': {'grabar_archivo': 1},
-       'grabar_archivo': {},
-       'leer_linea': {},
-       'leer_linea_string': {},
-       'ordenamiento_insercion': {},
-       'reemplazar_string': {},
-       'seccion_comentarios': {'unir_linea': 3},
-       'unir_linea': {},
-       'validar_linea': {'leer_linea': 4, 'reemplazar_string': 3, 'seccion_comentarios': 1, 'unir_linea': 3}}
+
+dic_prueba = {'analiza_codigo': {'generar_archivo': 2, 'leer_linea_string': 2, 'validar_linea': 1},
+           'generar_archivo': {'grabar_archivo': 1},
+           'grabar_archivo': {},
+           'leer_linea': {},
+           'leer_linea_string': {},
+           'ordenamiento_insercion': {},
+           'reemplazar_string': {},
+           'seccion_comentarios': {'unir_linea': 3},
+           'unir_linea': {},
+           'validar_linea': {'leer_linea': 4, 'reemplazar_string': 3, 'seccion_comentarios': 1, 'unir_linea': 3}}
 
 
-def busca_main():
+def busca_main(fuente_unico):
+    """[Autor: Sofia y Lucia]"""
+    """[Ayuda: Busca funciones que sea la función principal]"""
     main = []
-    fuente_unico = open("fuente_unico.csv", "r")
+    fuente_unico = open("fuente_unico.csv", "r") # Borrar y pasar como parametro
     dic = funciones_invocadas(fuente_unico)
     valores = list(dic.values())
-    
     for key in dic.keys():
-        while key not in main:
-            if key not in valores[0]:
+        contador = 0
+        while (len(valores) > contador) and (key not in list(valores[contador].keys())):
+            if (key not in main) and (contador == len(valores) - 1):
                 main.append(key)
-                
+            contador += 1
+
     return main
         
-busca_main()
