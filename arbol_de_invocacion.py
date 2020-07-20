a9b57a9847b95314fecb70d6fca3d346159c55b2
lucialiceri <63566999+lucialiceri@users.noreply.github.com>
2020-07-19 21:24:35 -0300

    Tratando de imprimir

--- arbol_de_invocacion.py
+++ arbol_de_invocacion.py
@@ -14,12 +14,10 @@ dic_prueba = {'analiza_codigo': {'generar_archivo': 2, 'leer_linea_string': 2, '
            'validar_linea': {'leer_linea': 4, 'reemplazar_string': 3, 'seccion_comentarios': 1, 'unir_linea': 3}}
 
 
-def busca_main(fuente_unico):
+def busca_main(dic):
     """[Autor: Sofia y Lucia]"""
     """[Ayuda: Busca funciones que sea la función principal]"""
-    main = []
-    fuente_unico = open("fuente_unico.csv", "r") # Borrar y pasar como parametro
-    dic = funciones_invocadas(fuente_unico)
+    main = []   
     valores = list(dic.values())
     for key in dic.keys():
         contador = 0
@@ -29,4 +27,25 @@ def busca_main(fuente_unico):
             contador += 1
 
     return main
+
+def cant_lineas(lista_ar_fu, dic):
+    """[Autor: Lucia]"""
+    """[Ayuda: Cuenta la cantidad de lineas por funcion]"""
+    
+    for funcion in lista_ar_fu:
+        dic[funcion[0]]["lineas"] = len(funcion) - 2
         
+    return dic
+
+def esquema(dic, lista_ar, main):
+    for key in dic.keys():
+        for funcion in lista_ar:
+            print("{}({}) --> {}  ".format(dic[main[0]], dic[main[0]]["lineas"], dic[main[0]][funcion[0]]))
+    
+fuente_unico = open("fuente_unico.csv", "r") # Borrar y pasar como parametro
+lista_ar = listar_archivo(fuente_unico)
+fuente_unico.close()
+fuente_unico = open("fuente_unico.csv", "r")
+diccionario = funciones_invocadas(fuente_unico)
+cant_lineas(lista_ar, diccionario)
+fuente_unico.close()
