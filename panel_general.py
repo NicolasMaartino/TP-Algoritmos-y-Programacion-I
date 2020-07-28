93d929ea0277c0fe23d9616e8bedfc4273821c9c
juanIgodoy <jigodoy@fi.uba.ar>
2020-07-28 13:12:38 -0300

    comillas panel

--- panel_general.py
+++ panel_general.py
@@ -14,20 +14,20 @@ def organizar_archivo(lista_ar):
     funciones = {}
     for funcion in lista_ar:
         funciones[funcion[0]] = {}
-        funciones[funcion[0]]["Nombre"] = "{}.{}".format(funcion[0], funcion[2])
-        funciones[funcion[0]]["Parametros"] = funcion[1].strip('()')
-        funciones[funcion[0]]["Lineas"] = len(funcion) - 3 #Por los parametros,el modulo y el nombre
-        funciones[funcion[0]]["Invocaciones"] = 0
-        funciones[funcion[0]]["return"] = 0
-        funciones[funcion[0]]["if"] = 0
-        funciones[funcion[0]]["elif"] = 0
-        funciones[funcion[0]]["for"] = 0
-        funciones[funcion[0]]["while"] = 0
-        funciones[funcion[0]]["break"] = 0
-        funciones[funcion[0]]["exit"] = 0
-        funciones[funcion[0]]["Coment"] = 0
-        funciones[funcion[0]]["Ayuda"] = ""
-        funciones[funcion[0]]["Autor"] = ""
+        funciones[funcion[0]]['Nombre'] = '{}.{}'.format(funcion[0], funcion[2])
+        funciones[funcion[0]]['Parametros'] = funcion[1].strip('()')
+        funciones[funcion[0]]['Lineas'] = len(funcion) - 3 #Por los parametros,el modulo y el nombre
+        funciones[funcion[0]]['Invocaciones'] = 0
+        funciones[funcion[0]]['return'] = 0
+        funciones[funcion[0]]['if'] = 0
+        funciones[funcion[0]]['elif'] = 0
+        funciones[funcion[0]]['for'] = 0
+        funciones[funcion[0]]['while'] = 0
+        funciones[funcion[0]]['break'] = 0
+        funciones[funcion[0]]['exit'] = 0
+        funciones[funcion[0]]['Coment'] = 0
+        funciones[funcion[0]]['Ayuda'] = ''
+        funciones[funcion[0]]['Autor'] = ''
     return funciones
 
 def contador (elementos, lista_ar, dic):
@@ -36,7 +36,7 @@ def contador (elementos, lista_ar, dic):
     for elemento in elementos:
         for funcion in lista_ar:
             for i in range(3, len(funcion)): 
-                dic[funcion[0]][elemento] += funcion[i].count(elemento + " ")
+                dic[funcion[0]][elemento] += funcion[i].count(elemento + ' ')
 
     return dic
 
@@ -44,11 +44,11 @@ def parametros(lista_ar, dic):
     """[Autor: Lucia]"""
     """[Ayuda: Cuenta la cantidad de parametros]"""
     for key in dic:
-        if dic[key]["Parametros"] == "":
-            dic[key]["Parametros"] = 0
+        if dic[key]['Parametros'] == '':
+            dic[key]['Parametros'] = 0
         else:
-            cant = dic[key]["Parametros"].count(" ")
-            dic[key]["Parametros"] = cant + 1
+            cant = dic[key]['Parametros'].count(' ')
+            dic[key]['Parametros'] = cant + 1
 
 def extraigo_linea(funcion,key,dic):
     """ [Autor : Lucia]
@@ -58,11 +58,11 @@ def extraigo_linea(funcion,key,dic):
     funcion = funcion[3:len(funcion)]
     for linea in funcion:
         linea = linea.split()
-        texto = ""+key+"("
+        texto = ''+key+'('
         for elemento in linea:
             final = elemento[0:len(texto)]
             if final == texto:
-                dic[key]["Invocaciones"] +=1 
+                dic[key]['Invocaciones'] +=1 
     return dic
 
 def invocaciones(lista_ar, dic):
@@ -71,7 +71,7 @@ def invocaciones(lista_ar, dic):
     for key in dic:
         # agarro una funcion
         for funcion in lista_ar:  
-            funcion = acomodar_lectura(funcion,["="]," = ")
+            funcion = acomodar_lectura(funcion,['='],' = ')
             extraigo_linea(funcion,key,dic)
     return dic
 
@@ -80,7 +80,7 @@ def lineas_coment(lista_ar, dic):
        [Ayuda: cuenta las lineas de comentarios que no sean de autor o ayuda]"""
     for funcion in lista_ar:
         if (len(funcion) > 3):
-            dic[funcion[0]]["Coment"] += len(funcion) -3
+            dic[funcion[0]]['Coment'] += len(funcion) -3
     return dic
 
 def ayuda(lista_ar, dic):
@@ -88,16 +88,16 @@ def ayuda(lista_ar, dic):
        [Ayuda: verifica si hay o no un comentario de ayuda dentro de la función]"""
     for funcion in lista_ar:
         if (funcion[2] == 'N/N'):
-            dic[funcion[0]]["Ayuda"] = "No"
+            dic[funcion[0]]['Ayuda'] = 'No'
         else:
-            dic[funcion[0]]["Ayuda"] = "Si"
+            dic[funcion[0]]['Ayuda'] = 'Si'
     return dic
 
 def autor(lista_ar, dic):
     """[Autor: Lucia]"""
     """[Ayuda: Indica el nombre del autor de la función]"""
     for funcion in lista_ar:
-        dic[funcion[0]]["Autor"] = funcion[1]
+        dic[funcion[0]]['Autor'] = funcion[1]
     return dic
 
 def unir(dic, lista_fu, lista_com):
@@ -114,14 +114,14 @@ def unir(dic, lista_fu, lista_com):
 def procesa_linea(valor,nombre,if_elif,archivo,valor_final):
     """[Autor : Nicolas]"""
     """[Ayuda : Funcion que graba la linea segun condiciones]"""
-    if nombre == "if" or nombre == "elif":
+    if nombre == 'if' or nombre == 'elif':
         valor_final += valor
         if_elif.remove(nombre)
         if len(if_elif) == 0:
-            leyenda = str(valor_final) + ","
+            leyenda = str(valor_final) + ','
             grabar_archivo(archivo,leyenda)
     else:            
-        leyenda = str(valor) + ","
+        leyenda = str(valor) + ','
         grabar_archivo(archivo,leyenda)
 
     return if_elif
@@ -130,19 +130,19 @@ def procesa_linea(valor,nombre,if_elif,archivo,valor_final):
 def panel_csv(dic):
     """[Autor : Nicolas]
        [Ayuda : Genera la escritura del archivo panel_general]"""
-    archivo = open("panel_general.csv","w")
-    lista = ["Funcion,","Parametros,","lineas,","invocaciones,","Returns,","If/Elif,","For,","While,","Break,","Exit,","Coment,","Ayuda,","Autor"]
+    archivo = open('panel_general.csv','w')
+    lista = ['Funcion,','Parametros,','lineas,','invocaciones,','Returns,','If/Elif,','For,','While,','Break,','Exit,','Coment,','Ayuda,','Autor']
     for elemento in lista:
         archivo.write(elemento)
-    archivo.write("\n")
+    archivo.write('\n')
     for funcion_principal in dic:
-        if_elif = ["if","elif"]
+        if_elif = ['if','elif']
         valor_final = 0
         for valores in dic[funcion_principal]:
             valor = dic[funcion_principal][valores]
             # Esta funcion ejecutara todo el proceso para meter la linea en el archivo
             if_elif = procesa_linea(valor,valores,if_elif,archivo,valor_final)
-        archivo.write("\n")
+        archivo.write('\n')
     archivo.close()
 
 
