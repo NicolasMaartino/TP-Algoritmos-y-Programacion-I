f69448bf045654f793bcadb3a7768084f2d80bbb
juanIgodoy <jigodoy@fi.uba.ar>
2020-07-28 13:27:24 -0300

    comillas reut

--- reutilizacion.py
+++ reutilizacion.py
@@ -7,12 +7,12 @@ def lista_de_funciones():
     los nombres de todas las funciones que se encuentran
     en el primer elemento de las lineas del archivo ]
     """
-    fuente=open("fuente_unico.csv","r")
-    linea = leer_linea_clasico(fuente,",")
+    fuente=open('fuente_unico.csv','r')
+    linea = leer_linea_clasico(fuente,',')
     funciones = []
-    while linea!=("","",""):
+    while linea!=('','',''):
         funciones.append(linea[0])
-        linea = leer_linea_clasico(fuente,",")
+        linea = leer_linea_clasico(fuente,',')
     fuente.close()
     return funciones
 
@@ -32,9 +32,9 @@ def veces_invocadas(linea):
         #recorro todas las funciones de la lista de funciones
         for palabras in linea[3:]:
             #recorro todos los elementos de la linea que lei antes 
-            if "{}(".format(funcion) in palabras and funcion not in cant_invocaciones:
+            if '{}('.format(funcion) in palabras and funcion not in cant_invocaciones:
                 cant_invocaciones[funcion] = 1
-            elif "{}(".format(funcion) in palabras and funcion in cant_invocaciones:
+            elif '{}('.format(funcion) in palabras and funcion in cant_invocaciones:
                 cant_invocaciones[funcion] += 1
     return cant_invocaciones
 
@@ -49,12 +49,12 @@ def funciones_invocadas(fuente):
     funcion1 {funcion2 : n veces} ,funcion 1 llama a funcion 2 n veces]
     
     """
-    linea = leer_linea_clasico(fuente,",")
+    linea = leer_linea_clasico(fuente,',')
     invocaciones = {}
-    while linea !=("","",""):
+    while linea !=('','',''):
         funcion_1 = linea[0]   
         invocaciones[funcion_1] = veces_invocadas(linea)
-        linea = leer_linea_clasico(fuente,",")
+        linea = leer_linea_clasico(fuente,',')
 
     return invocaciones
 
@@ -72,12 +72,12 @@ def crear_filas(invocaciones, funciones, total):
         nueva_fila.append(filas)  
         for funcion in invocaciones:
             if funciones[x-1] in invocaciones[funcion]: 
-                filas.append("{:^4}|".format("x"))
+                filas.append('{:^4}|'.format('x'))
             elif funcion in invocaciones[funciones[x-1]]:
-                filas.append("{:^4}|".format(invocaciones[funciones[x-1]][funcion]))
+                filas.append('{:^4}|'.format(invocaciones[funciones[x-1]][funcion]))
                 total[funcion] += invocaciones[funciones[x-1]][funcion]
             else:
-                filas.append("{:^4}|".format(""))
+                filas.append('{:^4}|'.format(''))
     
     return nueva_fila,total
 
@@ -87,10 +87,10 @@ def crear_tabla(invocaciones,tabla,funciones):
     [Ayuda : Empiezo a crear la tabla]
     """
     x = len(funciones)
-    primera_fila = ""
+    primera_fila = ''
     t = 0
     total = {}
-    nuevo = ""
+    nuevo = ''
     funciones = lista_de_funciones()
     
     for funcion in funciones:
@@ -98,22 +98,22 @@ def crear_tabla(invocaciones,tabla,funciones):
     nueva_fila,total = crear_filas(invocaciones, funciones,total)
     
     for i in range(1,x+1):
-        primera_fila += "{:^4}|".format(i)
-    tabla.write("-"*41 + "-----"*x + "\n")
-    tabla.write("|{:<40}|".format("FUNCIONES") + primera_fila + "\n")
+        primera_fila += '{:^4}|'.format(i)
+    tabla.write('-'*41 + '-----'*x + '\n')
+    tabla.write('|{:<40}|'.format('FUNCIONES') + primera_fila + '\n')
     #creo la primera fila con "funciones" y los numeros que indican las funciones en las columnas
         
     for filas,funcion in zip(nueva_fila,invocaciones.keys()):
         t+=1
-        tabla.write("|" + "-"*40 + "|----"*x + "|\n")
-        tabla.write("|{:<40}|".format(str(t)+ "-" + funcion.replace("$","")) +"".join(filas)+ "\n" )
-    tabla.write("|" + "-"*40 + "|----"*x + "|\n")
+        tabla.write('|' + '-'*40 + '|----'*x + '|\n')
+        tabla.write('|{:<40}|'.format(str(t)+ '-' + funcion.replace('$','')) +''.join(filas)+ '\n' )
+    tabla.write('|' + '-'*40 + '|----'*x + '|\n')
     #meto todas las filas creadas en crear_filas dentro de la tabla central
     
     for valor in total.values():
-        nuevo += "{:^4}|".format(str(valor))        
-    tabla.write("|{:40}|".format("Total Invocaciones")  + nuevo + "\n")
-    tabla.write("|" + "-"*40 + "|----"*x + "|\n")
+        nuevo += '{:^4}|'.format(str(valor))        
+    tabla.write('|{:40}|'.format('Total Invocaciones')  + nuevo + '\n')
+    tabla.write('|' + '-'*40 + '|----'*x + '|\n')
     #finalizo la tabla con la ultima fila que contiene el total de invocaciones
 
 def imprimir_analizador():
@@ -121,14 +121,14 @@ def imprimir_analizador():
     [Autor : Sofia Marchesini]
     [Ayuda : imprimo la tabla y la copio al archivo analizador.txt]
     """
-    fuente = open("fuente_unico.csv","r")
+    fuente = open('fuente_unico.csv','r')
     analizador = open('analizador.txt','w+')
     invocaciones = funciones_invocadas(fuente)
     funciones = lista_de_funciones()
     crear_tabla(invocaciones,analizador,funciones)
     analizador.seek(0)
     fila = analizador.readline().strip()
-    while fila != "":
+    while fila != '':
         print(fila)
         fila = analizador.readline().rstrip()
     analizador.close()
