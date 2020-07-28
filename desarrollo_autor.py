f32ee602fababcec6deca2bd2a8eb585547baa07
alejandrodelcs <alejodc15@gmail.com>
2020-07-28 16:41:49 -0300

    Version sayayin

--- desarrollo_autor.py
+++ desarrollo_autor.py
@@ -12,7 +12,6 @@ def info_desarrolladores(fuente_unico,comentarios):
         [Ayuda : nose ]"""
     total_lineas = 0
     dicc_desarrolladores = {}
-<<<<<<< HEAD
     linea_comentarios = leer_archivos_csv(comentarios)
     linea_fuente_unico = leer_archivos_csv(fuente_unico)
     while linea_comentarios and linea_fuente_unico:
@@ -23,20 +22,6 @@ def info_desarrolladores(fuente_unico,comentarios):
             dicc_desarrolladores[linea_comentarios[1].strip()] = [[linea_fuente_unico[0],len(linea_fuente_unico[3:])]]
         linea_fuente_unico = leer_archivos_csv(fuente_unico)
         linea_comentarios = leer_archivos_csv(comentarios)
-=======
-    with open('comentarios.csv','r') as comentarios,\
-        open('fuente_unico.csv','r') as fuente_unico:
-            linea_comentarios = leer_archivos_csv(comentarios)
-            linea_fuente_unico = leer_archivos_csv(fuente_unico)
-            while linea_comentarios and linea_fuente_unico:
-                total_lineas += len(linea_fuente_unico[3:])
-                if linea_comentarios[1].strip() in dicc_desarrolladores:
-                    dicc_desarrolladores[linea_comentarios[1].strip()].append([linea_fuente_unico[0],len(linea_fuente_unico[3:])])
-                else:
-                    dicc_desarrolladores[linea_comentarios[1].strip()] = [[linea_fuente_unico[0],len(linea_fuente_unico[3:])]]
-                linea_fuente_unico = leer_archivos_csv(fuente_unico)
-                linea_comentarios = leer_archivos_csv(comentarios)
->>>>>>> c8e36b4cf55748ecd258c56f2e12474110edabc0
     carga_informacion_desarrollador(dicc_desarrolladores,total_lineas)
     fuente_unico.close()
     comentarios.close()
