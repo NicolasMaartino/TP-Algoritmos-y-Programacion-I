97c32f84ada433a8b7bafb8f7efdd3e7215a1d79
alejandrodelcs <alejodc15@gmail.com>
2020-07-29 08:15:35 -0300

    basura

--- archivos_prueba/leer_archivos.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-def leer_archivos_csv(archivocsv):
-    """ Autor : Alejandro """
-    """ Ayuda : leer archivos .csv y devuelve una lista de cada linea del archivo """
-    linea = archivocsv.readline()
-    return linea.rstrip('\n').split(',') if linea else ""
